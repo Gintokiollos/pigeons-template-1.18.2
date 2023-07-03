deaacbacc37d54ecfa466f3523adf3a1ee767c51
PC2 <a2568585992@gmail.com>
2023-07-03 16:19:49 +0800

    1

---
+++ src/main/java/com/pigeons/pigeonsModClient.java
@@ -0,0 +1,11 @@
+package com.pigeons;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.apache.http.cookie.ClientCookie;
+
+public class pigeonsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/pigeons/pigeonsModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.pigeons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class pigeonsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
